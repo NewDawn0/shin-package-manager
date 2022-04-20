c68c6bf52f4d6ea01b40afa08fe0bcdf8ef07686
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-04-20 17:01:46 +0200

    made script failsave

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "starting install..."
-python  install/getOs.py
+id=$(id -u)
+if [ $id -eq 0 ]; then
+    echo "Do not run this script as root"
+else
+    echo "starting install..."
+    echo ""
+    bash install/user.sh
+    sudo python install/getOs.py
+fi
