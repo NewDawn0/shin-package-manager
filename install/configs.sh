1c2dd500372d2c2fd29ce79276fc41b742446ada
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-04-20 17:02:35 +0200

    removed username query

--- install/configs.sh
+++ install/configs.sh
@@ -6,7 +6,8 @@ echo -e "\033[0;36m   ___) | | | | | | | |\033[0m"
 echo -e "\033[0;36m  |____/|_| |_|_|_| |_|\033[0m"
 echo ""
 echo "Installing configs ..."
-this=$(pwd)
+this=$1
+echo $this
 cd $HOME
 sudo mkdir -p .ndos/shin
 sudo cp $this/configs/shin.config $HOME/.ndos/shin/
@@ -14,7 +15,5 @@ sudo cp $this/configs/shinVersion.config $HOME/.ndos/shin/
 sudo cp $this/shin /usr/local/bin
 sudo touch $HOME/.ndos/shin/packages.txt
 sudo touch $HOME/.ndos/shin/managers.txt
-echo "Please enter your username"
-read name
-name=$(echo $name | tr '[:upper:]' '[:lower:]')
-echo user=$name  >> $HOME/.ndos/shin/shin.config
+cd $this
+cat install/user.txt >> $HOME/.ndos/shin/shin.config
