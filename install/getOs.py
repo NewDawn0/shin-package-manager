8af660329c53a9d88c7fa89df63b79be97db1618
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-04-20 17:03:14 +0200

    added linux install support

--- install/getOs.py
+++ install/getOs.py
@@ -1,13 +1,53 @@
 from sys import platform
+import webbrowser
+from time import sleep
 import os
-platform == 'other'
+
+def getDist ():
+    print("1)   Arch and Arch based distributions")
+    print("2)   Debian and Debian based distributions")
+    print("")
+    print("     If you are unsure about your distribution check one of these links:")
+    print("     https://wiki.archlinux.org/title/Arch-based_distributions#Active")
+    print("     https://distrowatch.com/search.php?basedon=Debian")
+    print("")
+
+
 if platform != "win32":
     if platform == "darwin":
-        os.system('sudo bash install/configs.sh')
+        os.system('sudo bash install/configs.sh $(pwd)')
         os.system('sudo bash install/macos.sh')
+    elif platform == "linux":
+        print("Platform: linux (detected)")
+        print("Enter the type of linux distribution")
+        while True:
+            getDist()
+            value = input("Enter Number: ")
+            try:
+                intValue = int(value)
+                if intValue in (1, 2):
+                    break
+            except ValueError:
+                print("Enter a valid number")
+                continue
+        if intValue == 1:
+            print("os.system('sudo bash install/configs.sh')")
+            print("os.system('sudo bash install/arch.sh')")
+        elif intValue == 2:
+            print("os.system('sudo bash install/configs.sh')")
+            print("os.system('sudo bash install/debian.sh')")
+        else:
+            print("Something went wrong")
+            print("Aborting...")
+            exit
     else:
-       pass
+        print("Shin doesn't work on " + platform)
+        print("Consider swithing to a unix system")
+        sleep(5)
+        webbrowser.open("https://archlinux.org/download/")
 
 else:
-    print("Shin doesn't work for windows")
+    print("Shin doesn't work on windows")
     print("Consider swithing to a unix system")
+    sleep(5)
+    webbrowser.open("https://archlinux.org/download/")
